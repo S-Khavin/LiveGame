b2b067b4978dc862594f799c9364f66a3a2e8aa6
S-Khavin <khavin2004@gmail.com>
2024-04-29 20:20:37 +0530

    live player 1 state

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,38 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   RiveFile riveFile = await RiveFile.asset('assets/rive/rive.riv');
-  runApp(GameWidget(game: SumoGame(riveFile: riveFile),));
+  runApp(GameWidget(
+    game: SumoGame(riveFile: riveFile),
+  ));
 }
 
-class SumoGame extends FlameGame{
-  final databaseReference = FirebaseDatabase.instance.ref();
+class SumoGame extends FlameGame {
+  DatabaseReference xPos = FirebaseDatabase.instance.ref().child('x');
+  DatabaseReference yPos = FirebaseDatabase.instance.ref().child('y');
+  double x = 0;
+  double y = 0;
   final RiveFile riveFile;
   SumoGame({required this.riveFile});
   late Player player;
   @override
-  Future<void> onLoad() async{
-    player = Player(playerArtboard: riveFile.artboardByName('Player')!
-    );
+  Future<void> onLoad() async {
+    player = Player(playerArtboard: riveFile.artboardByName('Player')!);
+    xPos.onValue.listen((event) {
+      x = event.snapshot.value as double;
+      print(x);
+    });
+    yPos.onValue.listen((event) {
+      y = event.snapshot.value as double;
+      print(y);
+    });
     add(player);
+  }
+
+  @override
+  void update(double dt) {
+    player.position = Vector2(x, y);
+    super.update(dt);
   }
 }
--- lib/player.dart
+++ lib/player.dart
@@ -1,16 +1,9 @@
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flame/components.dart';
 import 'package:flame_rive/flame_rive.dart';
 
 class Player extends RiveComponent {
   final Artboard playerArtboard;
   late RiveComponent player;
-  DatabaseReference xPos = FirebaseDatabase.instance.ref().child('x');
-  DatabaseReference yPos = FirebaseDatabase.instance.ref().child('y');
-  DatabaseReference dbcount = FirebaseDatabase.instance.ref().child('count');
-  double x = 0;
-  double y = 0;
-  String count = "none";
   Player({required this.playerArtboard})
       : super(
           artboard: playerArtboard,
@@ -20,26 +13,10 @@ class Player extends RiveComponent {
   @override
   Future<void> onLoad() async {
     player = RiveComponent(artboard: playerArtboard);
-    xPos.onValue.listen((event) {
-      x = event.snapshot.value as double;
-      player.x = x;
-      print(x);
-    });
-    yPos.onValue.listen((event) {
-       y = event.snapshot.value as double;
-      player.y = y;
-    });
-      dbcount.onValue.listen((event) {
-      String count = event.snapshot.value.toString();
-      print(count);
-    });
   }
 
   @override
   void update(double dt) {
     super.update(dt);
-    player.position = Vector2(x, y);
-    print(x);
-    print(y);
   }
 }
