9e6932b65bba0b03b26c5681aac0fa1e4a2847b8
S-Khavin <khavin2004@gmail.com>
2024-04-29 22:03:55 +0530

    Multiplayer data with delay

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flame/palette.dart';
 import 'package:flame_rive/flame_rive.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -18,7 +21,8 @@ void main() async {
   ));
 }
 
-class SumoGame extends FlameGame {
+class SumoGame extends FlameGame with DragCallbacks {
+  late JoystickComponent joystick;
   DatabaseReference xPos = FirebaseDatabase.instance.ref().child('x');
   DatabaseReference yPos = FirebaseDatabase.instance.ref().child('y');
   double x = 0;
@@ -28,6 +32,14 @@ class SumoGame extends FlameGame {
   late Player player;
   @override
   Future<void> onLoad() async {
+    final knobPaint = BasicPalette.blue.withAlpha(200).paint();
+    final backgroundPaint = BasicPalette.blue.withAlpha(100).paint();
+    joystick = JoystickComponent(
+      knob: CircleComponent(radius: 30, paint: knobPaint),
+      background: CircleComponent(radius: 100, paint: backgroundPaint),
+      margin: const EdgeInsets.only(left: 40, bottom: 40),
+    );
+    add(joystick);
     player = Player(playerArtboard: riveFile.artboardByName('Player')!);
     xPos.onValue.listen((event) {
       x = event.snapshot.value as double;
@@ -43,6 +55,18 @@ class SumoGame extends FlameGame {
   @override
   void update(double dt) {
     player.position = Vector2(x, y);
+    player.position.add(joystick.delta * 10 * dt);
+    // updatePlayerPosition(player.x, player.y);
+       // Limit database updates to reduce load
+    if (x != player.position.x) {
+      x = player.position.x;
+      xPos.set(x);
+    }
+    if (y != player.position.y) {
+      y = player.position.y;
+      yPos.set(y);
+    }
     super.update(dt);
+
   }
 }
--- lib/player.dart
+++ lib/player.dart
@@ -1,7 +1,8 @@
 import 'package:flame/components.dart';
 import 'package:flame_rive/flame_rive.dart';
+import 'package:livegame/main.dart';
 
-class Player extends RiveComponent {
+class Player extends RiveComponent with HasGameRef<SumoGame> {
   final Artboard playerArtboard;
   late RiveComponent player;
   Player({required this.playerArtboard})
